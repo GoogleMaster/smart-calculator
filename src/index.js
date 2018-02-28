740eab021e6dc0bfe15bd32977adbb1f81b05819
Kjudisca Rasjudisca <xaneta@ya.ru>
2018-02-28 12:59:07 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,26 +1,43 @@
 class SmartCalculator {
   constructor(initialValue) {
     // your implementation
+      this.value = initialValue.toString();
   }
 
   add(number) {
     // your implementation
+      this.value += '+' + number;
+      return this;
   }
-  
+
   subtract(number) {
     // your implementation
+      this.value += '-' + number;
+      return this;
   }
 
   multiply(number) {
     // your implementation
+      this.value += '*' + number;
+      return this;
   }
 
   devide(number) {
     // your implementation
+      this.value += '/' + number;
+      return this;
   }
 
   pow(number) {
     // your implementation
+      this.value += '**' + number;
+      return this;
+  }
+
+  valueOf(){
+      //console.log(this.value);
+      //console.log(eval(this.value));
+      return eval(this.value);
   }
 }
 
